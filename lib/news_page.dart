924d9485b29a2f4742cdb63a6bfe0f93626848f9
mshaileshr@gmail.com <Yes@383840>
2020-06-16 18:52:27 +0530

    contentstack: ^0.1.0 added

--- lib/news_page.dart
+++ lib/news_page.dart
@@ -1,68 +1,54 @@
 import 'package:flutter/material.dart';
-import 'contentstack.dart';
+import 'package:contentstack/contentstack.dart' as contentstack;
 import 'news_details.dart';
 
 class NewsPage extends StatefulWidget {
-
   @override
   _NewsPageState createState() => _NewsPageState();
 }
 
 class _NewsPageState extends State<NewsPage> {
-
-  var newsList = [];
-
   bool isDataAvailable = false;
-
-  //using contentstack-dart-sdk to fetch the news content
-   _getNews() async {
-
-    // initialise contentstack by providing credentials as shown below. 
-     var stack = Contentstack.stack( apiKey: 'stack_api_key',
-         accessToken: 'stack_delivery_token', environment: 'stack_environment');
-
-    // get entry instance as shown below.
-    Entry entry = stack.contentType('news').entry();
-    // find all the entries as shown below.
-    // In case you want to filter the entries, do provide key value pair in find method as parameter
-    var response =  await entry.find({});
-    // checks if response is not null and not empty.
-    if (response!=null && response.isNotEmpty){
-      print(response.containsKey('entries'));
-      //parse entries from the response. as shown below.
-      var resp = response['entries'];
-      isDataAvailable = true;
-      // update the response so UI can update accordingly.
-      setState(() {
-        newsList = resp;
-      });
-    }
-  }
+  var newsList = [];
 
   @override
   void initState() {
     super.initState();
-    // get news response.
     _getNews();
   }
 
+  _getNews() async {
+    final stack = contentstack.Stack(
+        'blt7979d15c28261b93', 'cs17465ae5683299db9d259cb6', 'production');
+    final query = stack.contentType('news').entry().query();
+    await query.find().then((response) {
+      isDataAvailable = true;
+      setState(() {
+        newsList = response['entries'];
+      });
+    }).catchError((error) {
+      print(error.message.toString());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: Text('News', style: TextStyle(color: Colors.white, fontSize: 25, fontFamily: 'nunito' ),), centerTitle: true,
-      ),
-
-      body:  isDataAvailable ? Feeds(newsList: newsList) : Center(child: CircularProgressIndicator())
-    );
+        appBar: AppBar(
+          title: Text(
+            'News',
+            style: TextStyle(
+                color: Colors.white, fontSize: 25, fontFamily: 'nunito'),
+          ),
+          centerTitle: true,
+        ),
+        body: isDataAvailable
+            ? Feeds(newsList: this.newsList)
+            : Center(child: CircularProgressIndicator()));
   }
 }
 
-
 class Feeds extends StatelessWidget {
-
   const Feeds({
     Key key,
     @required this.newsList,
@@ -74,9 +60,8 @@ class Feeds extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: EdgeInsets.all(8),
-      itemCount: newsList.length,
+      itemCount: this.newsList.length,
       itemBuilder: (BuildContext context, int index) {
-
         return Card(
           elevation: 4,
           child: ListTile(
@@ -91,7 +76,8 @@ class Feeds extends StatelessWidget {
               );
             },
             title: Text(
-              newsList[index]['title'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              newsList[index]['title'],
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               maxLines: 1,
             ),
             leading: CircleAvatar(
@@ -102,7 +88,8 @@ class Feeds extends StatelessWidget {
             subtitle: Container(
               padding: EdgeInsets.all(4),
               child: Text(
-                newsList[index]['body'], style: TextStyle(fontSize: 15),
+                newsList[index]['body'],
+                style: TextStyle(fontSize: 15),
                 maxLines: 2,
               ),
             ),
